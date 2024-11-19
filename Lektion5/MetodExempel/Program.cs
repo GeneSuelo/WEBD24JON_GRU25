f8bf2b18064de58274487135174dda53ca92fb38
Nevena Kicanovic <111436933+Nina032@users.noreply.github.com>
2024-11-19 12:10:10 +0100

    24.11.19

---
+++ Lektion5/MetodExempel/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Till vilket tal vill du räkna med?");
+int valdaTal = ReadNumber();
+Count(valdaTal);
+
+
+void Count(int numberToCountTo)
+{
+    for (int i = 1; i <= numberToCountTo; i++)
+        Console.WriteLine(i);
+}
+
+int ReadNumber()
+{
+    string input = Console.ReadLine();
+    int number = int.Parse(input);
+    return number;
+}
+//Method overloading
+//Console.WriteLine("Programmet avslutas.");
+//Console.WriteLine(true);
+//Console.WriteLine(100);
---
+++ Lektion5/Ovning1/Program.cs
@@ -0,0 +1,71 @@
+﻿using static System.Array;
+using static System.Console;
+//Övning 1
+string[] lands = new string[] 
+            {
+                "Serbien",
+                "Kroatien",
+                "Grekland",
+                "Sverige",
+                "USA",
+                "Nederländerna"
+            };
+WriteLine("Innan sort: ");
+Utskrift(lands);
+WriteLine();
+
+Sort(lands);
+WriteLine("Efter sort: ");
+Utskrift(lands);
+WriteLine();
+
+Clear();
+
+//Övning 2
+
+int[] tals = { 5, 3, 8, 2, 7, 1, 4, 6 };
+WriteLine("Innan sort: ");
+UtskriftNum(tals);
+WriteLine();
+
+Sort(tals);
+
+WriteLine("Efter sort:");
+UtskriftNum(tals);
+WriteLine();
+
+Reverse(tals);
+
+WriteLine("Efter reverse: ");
+UtskriftNum(tals);
+Clear();
+
+WriteLine("____________________________________");
+//Övning 3
+Countdown(10);
+Clear();
+Countdown(50);
+
+void Countdown(int number)
+{
+    if (number == 0) return; //basfall
+ 
+    WriteLine(number);
+    Countdown(number - 1);
+}
+
+void Utskrift(string[] array)
+{
+    foreach (string e in array)
+    {
+        WriteLine(e);
+    }
+}
+
+void UtskriftNum(int[] array)
+{
+    foreach (int e in array)
+    {
+        WriteLine(e);
+    }
+}
