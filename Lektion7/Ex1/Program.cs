8ce5df0e2a1f6b7fc41010b8f5d7928f6d08fe1c
Nevena Kicanovic <111436933+Nina032@users.noreply.github.com>
2024-11-26 11:34:13 +0100

    24.11.26

---
+++ Lektion7/Ex1/Program.cs
@@ -0,0 +1,54 @@
+﻿//Skapar lista genom att instansiera klassen List<t>
+List<int> my_list = new List<int>();
+
+// Lägger till element i listan genom att använda Add-metoden
+my_list.Add(496);
+my_list.Add(100);
+my_list.Add(10);
+my_list.Add(10000);
+my_list.Add(1000);
+my_list.Add(100000);
+my_list.Add(1000);
+my_list.Add(0000);
+
+//Console.WriteLine(my_list); det går inte att skriva ut en lista på detta sätt
+
+Console.WriteLine("unsorted list:");
+PrintList(my_list); //Skriver ut listan
+
+//Sorterar listan
+my_list.Sort();
+Console.WriteLine("sorted list:");
+PrintList(my_list); //Skriver ut listan
+
+
+//Använd Reverse för att vända på listan
+my_list.Reverse();
+Console.WriteLine("Reversed list:");
+PrintList(my_list); //Skriver ut listan
+
+//Skriver ut antalet element i listan
+Console.WriteLine("Antal element i listan: " + my_list.Count);
+
+//Radera ett element i listan
+my_list.Remove(10);
+Console.WriteLine("Antal element i listan efter radering: " + my_list.Count);
+PrintList(my_list);
+
+//Använd RemovaAt för att radera ett element på en specifik position
+my_list.RemoveAt(2);
+Console.WriteLine("Antal element i listan efter radering (RemoveAt): " + my_list.Count);
+PrintList(my_list);
+
+//Använd Clear för att radera alla element i listan
+my_list.Clear();
+Console.WriteLine("Antal element i listan efter radering (Clear): " + my_list.Count);
+
+//Skapar en metod som skriver ut listan
+void PrintList(List<int> list)
+{
+    foreach (int i in list)
+    {
+        Console.WriteLine(i);
+    }
+}
---
+++ Lektion7/Ex2/Program.cs
@@ -0,0 +1,90 @@
+﻿//Skapar dictionary - collection-initializer syntax
+var cities = new Dictionary<string, string>()
+{
+    {"UK", "London, Manchester, Birmingham" },
+    {"USA", "Chicago, New York, Washington" },
+    {"India", "Mumbai,New Delhi, Pune" },
+    {"Sweden", "Stockholm, Gothenburg, Malmo" }
+};
+
+//Skriver ut alla ellements
+foreach (var city in cities)
+{
+    Console.WriteLine($"Key: {city.Key}, Value: {city.Value}");
+}
+Console.Clear();
+
+Console.WriteLine(cities["UK"]); //Skriver ut London, Manchester, Birmingham
+
+//använd ContainsKey("nyckel")
+if (cities.ContainsKey("USA"))
+{
+    Console.WriteLine("USA finns i dictionary");
+    Console.WriteLine(cities["USA"]);
+}
+else
+{
+    Console.WriteLine("USA finns inte i dictionary");
+}
+
+string result;
+//använd TryGetValue("nyckel", out var)
+if (cities.TryGetValue("Sweden", out result))
+{
+    Console.WriteLine("Din värde är:");
+    Console.WriteLine(result);
+}
+
+//Använder ElementAt för att skriva 
+for (int i = 0; i < cities.Count; i++)
+{
+    Console.WriteLine($"Key: {cities.ElementAt(i).Key}, Value: {cities.ElementAt(i).Value}");
+}
+
+Console.Clear();
+
+//Uppdatera element
+cities["UK"] = "Liverpool, Manchester, Birmingham";
+cities["USA"] = "Chicago, New York, Washington, Los Angeles";
+cities["France"] = "Paris, Marseille, Lyon"; 
+
+if (cities.ContainsKey("France"))
+{
+    Console.WriteLine("France finns i dictionary");
+    Console.WriteLine(cities["France"]);
+}
+else
+{
+    Console.WriteLine("France finns inte i dictionary");
+}
+
+//Ta bort element
+cities.Remove("France");
+cities.Remove("Serbien"); //Finns inte i dictionary men kastar inte exception
+
+if (cities.ContainsKey("France"))
+{
+    Console.WriteLine("France finns i dictionary");
+    Console.WriteLine(cities["France"]);
+}
+else
+{
+    Console.WriteLine("France finns inte i dictionary");
+}
+
+Console.Clear();
+
+foreach (var city in cities)
+{
+    Console.WriteLine($"Key: {city.Key}, Value: {city.Value}");
+}
+
+Console.Clear();
+
+cities.Clear(); //Raderar alla element i dictionary
+
+foreach (var city in cities)
+{
+    Console.WriteLine($"Key: {city.Key}, Value: {city.Value}");
+}
+
